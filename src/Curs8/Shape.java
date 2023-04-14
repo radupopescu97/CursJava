0258029a09e402a100bb2e9e32d7925d010dbf90
Radu Popescu <radu.popescu97@yahoo.com>
2023-04-14 21:37:52 +0300

    curs 8

---
+++ src/Curs8/Shape.java
@@ -0,0 +1,11 @@
+package Curs8;
+
+public class Shape {
+
+	public String nume = "nume generic";
+	public String culoare = "culoare generica";
+	
+	
+	
+	
+}
---
+++ src/Curs8/TestSquare.java
@@ -0,0 +1,19 @@
+package Curs8;
+
+public class TestSquare {
+
+	public static void main(String[] args) {
+		
+		Square patrat = new Square();
+		
+		//Shape patrat2 = new Square();
+		
+		System.out.println(patrat.nume);
+		System.out.println(patrat.culoare);
+
+		
+		
+		patrat.printDetails();
+	}
+
+}
